024eef3ec8243d2082e09e0e85fa4a2f34ed4075
HyunTaek Oh <oht222@naver.com>
2023-01-23 20:15:46 +0900

    add: Pretendard 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
