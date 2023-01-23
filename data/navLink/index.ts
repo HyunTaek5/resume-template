f697d7fa6f28be9f659b9bbaa73771d4b1e39e0b
HyunTaek Oh <oht222@naver.com>
2023-01-23 22:12:02 +0900

    add: GNB navLink 데이터 타입 추가

--- data/navLink/index.ts
+++ data/navLink/index.ts
@@ -1,4 +1,6 @@
-const navLinks = [
+import { navLink } from './types';
+
+const navLinks: navLink[] = [
   { id: 1, title: 'About', link: '/' },
   { id: 2, title: 'Projects', link: '/project' },
   { id: 3, title: 'Blog', link: '/blog' }
