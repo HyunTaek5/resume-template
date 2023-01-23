21b3e1a74a241e29212a0175ec1609776d85bfef
HyunTaek Oh <oht222@naver.com>
2023-01-23 20:16:50 +0900

    add: main App에 Head 셋업 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React, { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type Page<P = Record<string, never>> = NextPage<P> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type Props = AppProps<{ dehydratedState: unknown }> & {
+  Component: Page<{ dehydratedState: unknown }>;
+};
+
+function App({ Component, pageProps }: Props) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <div className='flex min-h-screen flex-1 flex-col'>
+        <Head>
+          <title>이력서 템플릿</title>
+          <meta name='description' content='Resume Template of Techeer' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+
+        {getLayout(<Component {...pageProps} />)}
+      </div>
+    </>
+  );
 }
+
+export default App;
