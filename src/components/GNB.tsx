4ea9ee249e629b365d4e2ca97b9ebc1a10bd77c4
HyunTaek Oh <oht222@naver.com>
2023-01-25 00:15:41 +0900

    add: GNB 클릭시 해당 route 일 때, GNB 버튼 아래에 언더바 추가

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -9,9 +9,14 @@ const GNB = () => {
     <nav className='w-full max-w-[270px] flex place-items-center justify-between mx-5 h-20 md:mx-40 '>
       {
         navLinks.map((navLink) => (
-          <button className='px-3 text-base font-medium' key={navLink.id} onClick={() => {
-            router.push(navLink.link);
-          }}>{navLink.title}</button>
+          <div key={navLink.id} className='flex flex-col max-w-min mt-2'>
+            <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-semibold' : 'font-medium'}`}
+                    onClick={() => {
+                      router.push(navLink.link);
+                    }}>{navLink.title}
+            </button>
+            <div className={`${router.asPath === navLink.link ? 'bg-main-1' : 'bg-white'} w-full h-2 rounded-md`} />
+          </div>
         ))
       }
     </nav>
