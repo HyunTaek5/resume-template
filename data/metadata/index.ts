79b274fd3d832e8310978780fb24e7eca41a82a3
HyunTaek Oh <oht222@naver.com>
2023-01-24 12:17:01 +0900

    add: metadata 데이터 추가

---
+++ data/metadata/index.ts
@@ -0,0 +1,18 @@
+const metadata = {
+  title: '이력서 템플릿',
+  metaList: [
+    {
+      id: 1,
+      name: 'description',
+      content: 'Resume Template of Techeer'
+    },
+    {
+      id: 2,
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }
+  ],
+  link: { rel: 'icon', href: '/favicon.ico' }
+};
+
+export default metadata;
