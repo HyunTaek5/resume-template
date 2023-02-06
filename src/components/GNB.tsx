2bd2781dd9db800db6f6aa8dc0806968258b49b4
HyunTaek Oh <oht222@naver.com>
2023-02-07 03:03:41 +0900

    add: GNB에 logo 예시 및 이름 추가

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -1,26 +1,36 @@
 import { useRouter } from 'next/router';
 
+import logo from '../../data/logo';
 import navLinks from '../../data/navLink';
+import AppleSVG from '../../public/icons/apple.svg';
 
 const GNB = () => {
   const router = useRouter();
 
   return (
-    <nav className='w-full max-w-[270px] flex place-items-center justify-between mx-5 h-20 md:mx-40 '>
-      {
-        navLinks.map((navLink) => (
-          <div key={navLink.id} className='flex flex-col max-w-min mt-2'>
-            <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-semibold' : 'font-medium'}`}
-                    onClick={() => {
-                      router.push(navLink.link);
-                    }}>{navLink.title}
-            </button>
-            <div
-              className={`${router.asPath === navLink.link ? 'bg-main-1' : 'bg-white'} w-full h-2 rounded-md mt-1`} />
-          </div>
-        ))
-      }
-    </nav>
+    <div className='flex'>
+      <nav className='w-full flex self-center ml-10 md:ml-44' onClick={() => {
+        router.push('/');
+      }}>
+        <AppleSVG className='h-9 w-fit cursor-pointer md:h-10 pr-3' />
+        <button className={'font-bold text-2xl hidden md:block mt-1'}>{logo.name}</button>
+      </nav>
+      <nav className='w-full max-w-[520px] flex place-items-center justify-between mx-10 h-20 md:mx-44 '>
+        {
+          navLinks.map((navLink) => (
+            <div key={navLink.id} className='flex flex-col max-w-min mt-2'>
+              <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-semibold' : 'font-medium'}`}
+                      onClick={() => {
+                        router.push(navLink.link);
+                      }}>{navLink.title}
+              </button>
+              <div
+                className={`${router.asPath === navLink.link ? 'bg-main-1' : 'bg-white'} w-full h-2 rounded-md mt-1`} />
+            </div>
+          ))
+        }
+      </nav>
+    </div>
   );
 };
 
