0b5ecb7f421ede76ce04eaa1f8a69cbeaa6df9d2
HyunTaek Oh <oht222@naver.com>
2023-02-07 03:29:43 +0900

    add: GNB 반응형 처리

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -9,23 +9,22 @@ const GNB = () => {
 
   return (
     <div className='flex'>
-      <nav className='w-full flex self-center ml-10 md:ml-44' onClick={() => {
+      <nav className='w-full flex self-center ml-10 tablet:ml-44' onClick={() => {
         router.push('/');
       }}>
-        <AppleSVG className='h-9 w-fit cursor-pointer md:h-10 pr-3' />
-        <button className={'font-bold text-2xl hidden md:block mt-1'}>{logo.name}</button>
+        <AppleSVG className='h-9 w-fit cursor-pointer tablet:h-10 pr-3' />
+        <button className={'font-bold text-md hidden tablet:block text-2xl mt-1'}>{logo.name}</button>
       </nav>
-      <nav className='w-full max-w-[520px] flex place-items-center justify-between mx-10 h-20 md:mx-44 '>
+      <nav
+        className='w-full max-w-[520px] flex place-items-center justify-between mx-10 h-20 tablet:mx-44'>
         {
           navLinks.map((navLink) => (
-            <div key={navLink.id} className='flex flex-col max-w-min mt-2'>
+            <div key={navLink.id} className='flex flex-col max-w-min hidden sm:block'>
               <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-semibold' : 'font-medium'}`}
                       onClick={() => {
                         router.push(navLink.link);
                       }}>{navLink.title}
               </button>
-              <div
-                className={`${router.asPath === navLink.link ? 'bg-main-1' : 'bg-white'} w-full h-2 rounded-md mt-1`} />
             </div>
           ))
         }
