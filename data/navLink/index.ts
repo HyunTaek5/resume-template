53488881e1b5bdc620e14f75a64b4ffc8e90f98e
HyunTaek Oh <oht222@naver.com>
2023-02-07 03:03:52 +0900

    add: GNB 버튼에 Home 추가

--- data/navLink/index.ts
+++ data/navLink/index.ts
@@ -1,9 +1,10 @@
 import { navLink } from './types';
 
 const navLinks: navLink[] = [
-  { id: 1, title: 'About', link: '/about' },
-  { id: 2, title: 'Projects', link: '/project' },
-  { id: 3, title: 'Blog', link: '/blog' }
+  { id: 1, title: 'Home', link: '/' },
+  { id: 2, title: 'About', link: '/about' },
+  { id: 3, title: 'Projects', link: '/project' },
+  { id: 4, title: 'Blog', link: '/blog' }
 ];
 
 export default navLinks;
