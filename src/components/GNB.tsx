a218b1071a4601241a9d5033d4c9ad4c5a44e1f6
HyunTaek Oh <oht222@naver.com>
2023-02-07 03:49:49 +0900

    add: GNB tablet 사이즈로 축소시 사이드바 아이콘 뜨게 수정

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -1,25 +1,26 @@
 import { useRouter } from 'next/router';
+import AppleSVG from 'public/icons/apple.svg';
+import MenuSVG from 'public/icons/icon-menu.svg';
 
 import logo from '../../data/logo';
 import navLinks from '../../data/navLink';
-import AppleSVG from '../../public/icons/apple.svg';
 
 const GNB = () => {
   const router = useRouter();
 
   return (
-    <div className='flex'>
-      <nav className='w-full flex self-center ml-10 tablet:ml-44' onClick={() => {
+    <div className='flex h-20 tablet:mx-44'>
+      <nav className='w-full flex self-center ml-10' onClick={() => {
         router.push('/');
       }}>
         <AppleSVG className='h-9 w-fit cursor-pointer tablet:h-10 pr-3' />
         <button className={'font-bold text-md hidden tablet:block text-2xl mt-1'}>{logo.name}</button>
       </nav>
       <nav
-        className='w-full max-w-[520px] flex place-items-center justify-between mx-10 h-20 tablet:mx-44'>
+        className='w-full max-w-[520px] flex place-items-center justify-between mx-10 h-20 '>
         {
           navLinks.map((navLink) => (
-            <div key={navLink.id} className='flex flex-col max-w-min hidden sm:block'>
+            <div key={navLink.id} className='flex flex-col max-w-min hidden tablet:block'>
               <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-semibold' : 'font-medium'}`}
                       onClick={() => {
                         router.push(navLink.link);
@@ -29,6 +30,9 @@ const GNB = () => {
           ))
         }
       </nav>
+      <div className='flex h-20 mr-10 place-items-center justify-end tablet:hidden'>
+        <MenuSVG className='h-fit w-fit' />
+      </div>
     </div>
   );
 };
