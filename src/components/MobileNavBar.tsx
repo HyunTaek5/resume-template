e36068e21c43d2abb3edc2f2c776769438819836
HyunTaek Oh <oht222@naver.com>
2023-02-19 22:49:23 +0900

    add: 모바일 네비게이션바 추가

---
+++ src/components/MobileNavBar.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+
+import { navLink } from '../../data/header/navLink/types';
+import useMobileNavBarStore from '../plugins/zustand/MobileNavBarStore';
+
+interface MobileNavBarProps {
+  navLinkList: navLink[];
+}
+
+function MobileNavBar({ navLinkList }: MobileNavBarProps) {
+  const router = useRouter();
+
+  const { isOpen } = useMobileNavBarStore();
+
+  return (
+    <>
+      <nav
+        className={`relative w-full h-screen flex flex-col place-items-center hidden ${isOpen && 'block'}`}>
+        {
+          navLinkList.map((navLink) => (
+            <div key={navLink.id} className={`flex flex-col max-w-min`}>
+              <button className={`px-3 text-base ${router.asPath === navLink.link ? 'font-bold' : 'font-semibold'}`}
+                      onClick={() => {
+                        router.push(navLink.link);
+                      }}>{navLink.title}
+              </button>
+            </div>
+          ))
+        }
+      </nav>
+    </>
+
+  );
+}
+
+export default MobileNavBar;
