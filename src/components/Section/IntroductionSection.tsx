ace2d9f34ba3ff30ba45b8adc3edcc735d98f3ef
HyunTaek Oh <oht222@naver.com>
2023-03-09 03:04:41 +0900

    add: IntroductionSection에 Contact Me 버튼 추가

--- src/components/Section/IntroductionSection.tsx
+++ src/components/Section/IntroductionSection.tsx
@@ -7,13 +7,14 @@ interface IntroductionProps {
 }
 
 function IntroductionSection({ introduction }: IntroductionProps) {
-  
+
   return (
     <>
       <div className='flex flex-col space-y-4'>
         <p>{introduction.jobTitle}</p>
         <text key='introduction-name' className='font-bold text-7xl text-main-1'>{introduction.name}</text>
         <text key='introduction-short' className='text-left'>{introduction.shortIntroduction}</text>
+        <button className='bg-main-1 text-white rounded-md max-w-[134px] h-11'>Contact Me</button>
       </div>
     </>
   );
