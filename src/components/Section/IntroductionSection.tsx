162ac13835c628ef1bbd51c9819a3fb4ba6c910c
HyunTaek Oh <oht222@naver.com>
2023-03-13 01:25:22 +0900

    add: IntroductionSection p, button 태그 키 추가

--- src/components/Section/IntroductionSection.tsx
+++ src/components/Section/IntroductionSection.tsx
@@ -11,10 +11,10 @@ function IntroductionSection({ introduction }: IntroductionProps) {
   return (
     <>
       <div className='flex flex-col space-y-4'>
-        <p>{introduction.jobTitle}</p>
+        <p key='introduction-job-title' className='font-tinos text-black'>{introduction.jobTitle}</p>
         <text key='introduction-name' className='font-bold text-7xl text-main-1'>{introduction.name}</text>
         <text key='introduction-short' className='text-left'>{introduction.shortIntroduction}</text>
-        <button className='bg-main-1 text-white rounded-md max-w-[134px] h-11'>Contact Me</button>
+        <button key='contact-button' className='bg-main-1 text-white rounded-md max-w-[134px] h-11'>Contact Me</button>
       </div>
     </>
   );
